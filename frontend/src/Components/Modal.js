313740de891472926b1c1d7a02ced6ea73966568
Themika <Weerasuriyathemik@gmail.com>
2024-08-30 14:56:40 -0400

    chore: Update Modal component with fake event data and improve event info layout

--- frontend/src/Components/Modal.js
+++ frontend/src/Components/Modal.js
@@ -4,22 +4,30 @@ import "../Styles/Component Styles/modal.css";
 const Modal = ({ isOpen, selectedEventDate, closeModal }) => {
   if (!isOpen) return null;
 
+  // Example fake event data
+  const fakeEventData = {
+    title: "Tech Conference 2024",//Required
+    guests: ["John Doe", "Jane Smith"], //Conditional if there is any
+    date: "January 1, 2023",//Required
+    description: `Join us for an exciting tech conference where industry leaders discuss the latest trends in technology.
+      Topics include AI, blockchain, and more.`,//Required
+    image: "https://via.placeholder.com/150",//Required
+  };
+
   return (
     <div className="modal">
       <div className="modal-content">
-        <div className="image-container">
-            {/* Image goes here */}
-        </div>
+        <div className="image-container"></div>
         <div className="event-info">
-          <p>Date: {selectedEventDate}</p>
-          <p>Guests: <span>Guest 1</span></p>
+          <div className="event-info-header">
+            <h2>{fakeEventData.title}</h2>
+            <p className="event-info-date">Date: {fakeEventData.date}</p>
+          </div>
+          <p className="event-info-guest">Guests: John Doe, Jane Smith</p>
         </div>
         <div className="description">
-          <h2>Event Description: </h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet
-            sollicitudin ipsum. Integer at lacus vitae felis sodales tincidunt.
-          </p>
+          <h2>Event Description</h2>
+          <p>{fakeEventData.description}</p>
         </div>
         <button onClick={closeModal}>Close</button>
       </div>
