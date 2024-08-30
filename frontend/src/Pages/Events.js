8dee74c2c2917819ed9f5334482ebc50fcb4f64e
Themika <Weerasuriyathemik@gmail.com>
2024-08-29 22:26:57 -0400

    chore: Update Events page with recent events cards and lazy loading for images

--- frontend/src/Pages/Events.js
+++ frontend/src/Pages/Events.js
@@ -83,9 +83,9 @@ const EventsPage = () => {
   };
 
   const closeModal = () => {
-    const modalElement = document.querySelector('.modal');
+    const modalElement = document.querySelector(".modal");
     if (modalElement) {
-      modalElement.classList.add('closing');
+      modalElement.classList.add("closing");
       setTimeout(() => {
         setIsModalOpen(false);
         setSelectedEventDate(null);
@@ -123,6 +123,12 @@ const EventsPage = () => {
                   className="recent-events-container"
                   ref={recentEventsContainerRef}
                 >
+                  <span>
+                    <TinyCard />
+                  </span>
+                  <span>
+                    <TinyCard />
+                  </span>
                   <span>
                     <TinyCard />
                   </span>
@@ -203,8 +209,21 @@ const EventsPage = () => {
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
-            <h2>Event Details</h2>
-            <p>Details for event on date: {selectedEventDate}</p>
+            <div className="image-container">
+            </div>
+            <div className="event-info">
+              <p>Date: {selectedEventDate}</p>
+              <p>Guests: <span>Guest 1</span></p>
+              
+            </div>
+            <div className="description">
+              <h2>Event Description: </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Nulla sit amet sollicitudin ipsum. Integer at
+                lacus vitae felis sodales tincidunt.
+              </p>
+            </div>
             <button onClick={closeModal}>Close</button>
           </div>
         </div>
